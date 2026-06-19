43271e6bdff70d2fb3ba43427803c9e8cb7242c0
shalom dahan <vdumpa972@gmail.com>
2026-06-18 22:16:16 -0700

    Fix iOS bundle identifier

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.prayercompanion.app',
+  appId: 'com.vdumpa972.prayercompanion',
   appName: 'Prayer Companion',
   webDir: 'public',
   server: {
